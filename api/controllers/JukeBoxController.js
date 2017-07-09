864399327851f5723f846a31182465e4efd6a4e0
William T Oliver <WilliamTOliver@outlook.com>
2017-07-08 20:53:52 -0500

    debugging spotify oauth

---
+++ api/controllers/JukeBoxController.js
@@ -0,0 +1,60 @@
+/**
+ * JukeBoxController
+ *
+ * @description :: Server-side logic for SyntheticJukeBox Spotify requests
+ * @help        :: See Readme & https://developer.spotify.com/web-api/authorization-guide/
+ */
+var request = require('request');
+
+
+module.exports =
+    {
+        getCurrentJBPlaylist: function (req, res) {
+            /**
+            * Send Get Request to SpotifyServer to Get User Playlists
+            */
+            var user_id = 1290172871;
+            var playlist_id = '2HeNIdna5lLztR4wb6U4IJ';
+            var playlistURL = 'https://api.spotify.com/v1/users/' + user_id + '/playlists/' + playlist_id + '/tracks';
+
+            AuthService.doAuth(req, res).then(function () {
+                if (req.session.auth) {
+                    var auth_token = req.session.auth;
+                    var options = {
+                        url: playlistURL,
+                        headers: { 'Authorization': 'Bearer ' + auth_token },
+                        json: true
+                    };
+                    request.get(options, function (err, res) {
+                        if (err) {
+                            return res.serverError(err)
+                        }
+                        if (res) {
+                            var data = res.body;
+                            console.log(data);
+
+                        }
+                    })
+                }
+            });
+
+
+            // JukeBoxService.getCurrentJBPlaylist(req, res);
+        },
+        saveNewPlaylist: function (req, res) {
+            /**
+            * Initialize OAuth Process
+            */
+            var path = req.allParams().path;
+
+            AuthService.doAuth(req, res);
+        },
+        getSongsFromDB: function (req, res) {
+            /**
+            * Initialize OAuth Process
+            */
+            var path = req.allParams().path;
+
+            AuthService.doAuth(req, res);
+        }
+    }
--- api/services/AuthService.js
+++ api/services/AuthService.js
@@ -6,27 +6,22 @@
  */
 
 
-var request = require('request'); // "Request" library
+var request = require('request');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = '7b5b90442c8a4b0d9812dd41ca68130b'; 
-var client_secret = 'cd9a25a5f05a4c7dbdc1d7424ae642c4'; 
+var client_id = '7b5b90442c8a4b0d9812dd41ca68130b';
+var client_secret = 'cd9a25a5f05a4c7dbdc1d7424ae642c4';
 var redirect_uri = 'http://localhost:1337/auth/authCallback';
 var stateKey = 'spotify_auth_state';
- 
-
-
-
-
 
 module.exports = {
 
-    doAuth: async function (req, res) {
-    /**
-     * Spotify OAuth:
-     * Step 1: Get a secret to exchange server side for the auth-token.
-     */
+    doAuth: function (req, res) {
+        /**
+         * Spotify OAuth:
+         * Step 1: Get a secret to exchange server side for the auth-token.
+         */
         var generateRandomString = function (length) {
             /**
             * Generates a random string containing numbers and letters
@@ -41,11 +36,11 @@ module.exports = {
             return text;
         }
 
-        var state = await generateRandomString(16);
+        var state = generateRandomString(16);
         res.cookie(stateKey, state);
         var scope = 'user-read-private user-read-email';
 
-        // Request Secret
+        // Request Token
         res.redirect('https://accounts.spotify.com/authorize?' +
             querystring.stringify({
                 response_type: 'code',
@@ -54,11 +49,24 @@ module.exports = {
                 redirect_uri: redirect_uri,
                 state: state
             }));
+        return setTimeout(function(){
+            return res.send(req.session)
+        },1000);
     },
     getAuthTokenFromCode: function (req, res) {
         /**
         * Handle initial OAuth callback
         */
+        var sendAuth = function (err, data) {
+            /**
+            * Return Result
+            */
+            if (err) {
+                return res.serverError(err)
+            }
+            req.session.auth = data;
+            return res.send(req.session);
+        }
         var code = req.query.code || null;
         var state = req.query.state || null;
         var storedState = req.cookies ? req.cookies[stateKey] : null;
@@ -80,56 +88,75 @@ module.exports = {
                 },
                 json: true
             };
-
+            var response_object = {};
             request.post(authOptions, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
+                if (error) {
+                    return res.serverError(error);
+                }
+                if (response.statusCode === 200) {
                     var access_token = body.access_token,
                         refresh_token = body.refresh_token;
+
                     var options = {
                         url: 'https://api.spotify.com/v1/me',
                         headers: { 'Authorization': 'Bearer ' + access_token },
                         json: true
                     };
-                    var response_object = {};
-                    response_object.options = options;
-                    response_object.refresh_token = refresh_token
-                    // use the access token to access the Spotify Web API
-                    // request.get(options, function (error, response, body) {
-                    //     if (error){
-                    //         return
-                    //     }
-                    //     if(body){
-                    //     var data = body
-                    //     }
-                    // }).then(function(data){
-                    //     if(data){
-                    //         return res.json(data);
-                    //     }
-                    // });
-                    
-
-                    // // we can also pass the token to the browser to make requests from there
+                    sendAuth(null, {access_token: access_token});
                     // res.redirect('/#' +
                     //     querystring.stringify({
+                    //         allisgood: 'bam',
                     //         access_token: access_token,
                     //         refresh_token: refresh_token
-                    // }));
-                    if (response_object){
-                        
-                    }
-                } else {
-                    res.redirect('/#' +
-                        querystring.stringify({
-                            error: 'invalid_token'
-                        }));
+                    //     }))
                 }
             });
-            
         }
-
     },
-    refreshToken: function (req, res) {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO Integrate refresh token functionality
+    refreshToken: function (req, res) {
+        //EXCESS CODE 
+        // var options = {
+        //     url: 'https://api.spotify.com/v1/me',
+        //     headers: { 'Authorization': 'Bearer ' + access_token },
+        //     json: true
+        // };
+        // response_object.options = options;
+        // response_object.refresh_token = refresh_token
+        // // use the access token to access the Spotify Web API
+        // if (req.params.endpoint == 'jb-playlist') {
+        //     options.url = req.params.playlist;
+        //     request.get(options, function (err, res) {
+        //         if (err) {
+        //             return res.serverError(err)
+        //         }
+        //         if (res) {
+        //             var data = res.body;
+
+        //         }
+        //     })
+        // }
+        // // we can also pass the token to the browser to make requests from there
+        // res.redirect('/#' +
+        //     querystring.stringify({
+        //         access_token: access_token,
+        //         refresh_token: refresh_token
+        // }));
 
         var refresh_token = req.query.refresh_token;
         var authOptions = {
