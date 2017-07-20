69adb4dc33b7dd2684965f796b4d933206acd028
William T Oliver <WilliamTOliver@outlook.com>
2017-07-20 15:24:13 -0500

    clean up after mvp

--- api/controllers/JukeBoxController.js
+++ api/controllers/JukeBoxController.js
@@ -9,46 +9,28 @@ var request = require('request');
 
 module.exports =
     {
+        // CREATE
         createAttribute: function (req, res) {
-            /**
-            * Initialize OAuth Process
-            */
             JukeBoxService.createAttribute(req, res);
         },
         createGenre: function (req, res) {
-            /**
-            * Initialize OAuth Process
-            */
             JukeBoxService.createGenre(req, res);
         },
         createPlaylist: function (req, res) {
-            /**
-            * Initialize OAuth Process
-            */
             JukeBoxService.createPlaylist(req, res);
         },
-        getSongs: function (req, res) {
-            /**
-            * Initialize OAuth Process
-            */
-            JukeBoxService.getSongs(req, res);
+        createSong: function (req, res) {
+            JukeBoxService.createSong(req, res);
         },
+        // GET
+        
         getAttributes: function (req, res) {
-            /**
-            * return a list of genres in the mongo db
-            */
             JukeBoxService.getAttributes(req, res);
         },
         getGenres: function (req, res) {
-            /**
-            * return a list of genres in the mongo db
-            */
             JukeBoxService.getGenres(req, res);
         },
-        createSong: function (req, res) {
-            /**
-            * Initialize OAuth Process
-            */
-            JukeBoxService.createSong(req, res);
-        },
+        getSongs: function (req, res) {
+            JukeBoxService.getSongs(req, res);
+        }
     }
